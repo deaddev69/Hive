787629b638a7eb3bb4d77eb1b13b1ff131618148
deaddev69 <mail2nirmalvn@gmail.com>
2026-07-21 08:52:49 +0530

    fix(pwa): remove custom runtimeCaching causing _async_to_generator ReferenceError in sw.js - use library defaults with cacheId hive-v3 for full cache eviction

--- apps/customer/next.config.ts
+++ apps/customer/next.config.ts
@@ -7,68 +7,8 @@ const withPWA = withPWAInit({
   dynamicStartUrl: false,
   cacheStartUrl: false,
   workboxOptions: {
-    cacheId: "hive-v2",
+    cacheId: "hive-v3",
     cleanupOutdatedCaches: true,
-    runtimeCaching: [
-      {
-        urlPattern: /\/_next\/image\?url=.+$/i,
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "next-image-v2",
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/(?:cdn\.hivenow\.in|.*\.convex\.cloud|images\.unsplash\.com)\/.*/i,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "external-media-v2",
-          networkTimeoutSeconds: 3,
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "static-image-assets-v2",
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/.*/i,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "cross-origin-v2",
-          networkTimeoutSeconds: 3,
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
   },
 });
 
